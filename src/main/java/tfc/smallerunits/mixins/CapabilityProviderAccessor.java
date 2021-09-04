9f6007f671da65b7d7af5e9dcc49bf9477bf2b1c
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-09-04 17:38:13 +0300

    Remove unneeded uses of Unsafe

---
+++ src/main/java/tfc/smallerunits/mixins/CapabilityProviderAccessor.java
@@ -0,0 +1,20 @@
+package tfc.smallerunits.mixins;
+
+import net.minecraftforge.common.capabilities.CapabilityDispatcher;
+import net.minecraftforge.common.capabilities.CapabilityProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(CapabilityProvider.class)
+public interface CapabilityProviderAccessor {
+    @Accessor
+    @Mutable
+    void setBaseClass(Class<?> baseClass);
+
+    @Accessor
+    void setCapabilities(CapabilityDispatcher capabilities);
+
+    @Accessor
+    void setValid(boolean valid);
+}
---
+++ src/main/java/tfc/smallerunits/mixins/ServerWorldAccessor.java
@@ -0,0 +1,12 @@
+package tfc.smallerunits.mixins;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.util.WorldCapabilityData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ServerWorld.class)
+public interface ServerWorldAccessor {
+    @Accessor
+    void setCapabilityData(WorldCapabilityData capabilityData);
+}
