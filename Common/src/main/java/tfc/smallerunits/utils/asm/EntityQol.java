39d411819cb4c2d45821f5fe95b0727266eccab4
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2023-12-31 23:48:06 -0500

    huh.

--- Common/src/main/java/tfc/smallerunits/utils/asm/EntityQol.java
+++ Common/src/main/java/tfc/smallerunits/utils/asm/EntityQol.java
@@ -9,15 +9,16 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkStatus;
+import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import tfc.smallerunits.data.storage.RegionPos;
+import tfc.smallerunits.simulation.chunk.BasicVerticalChunk;
 import tfc.smallerunits.simulation.level.ITickerLevel;
 import tfc.smallerunits.utils.math.HitboxScaling;
 
-import java.util.Map;
 import java.util.Set;
 import java.util.function.BiConsumer;
 
@@ -44,23 +45,43 @@ public class EntityQol {
 		Vec3 center = box.getCenter();
 		AABB aabb = HitboxScaling.getOffsetAndScaledBox(box, new Vec3(center.x, box.minY, center.z), ((ITickerLevel) level).getUPB(), regionPos);
 		
-		int i = Mth.floor(aabb.minX);
-		int j = Mth.ceil(aabb.maxX);
-		int k = Mth.floor(aabb.minY);
-		int l = Mth.ceil(aabb.maxY);
-		int i1 = Mth.floor(aabb.minZ);
-		int j1 = Mth.ceil(aabb.maxZ);
+		int minX = Mth.floor(aabb.minX);
+		int minY = Mth.ceil(aabb.maxX);
+		int minZ = Mth.floor(aabb.minY);
+		int maxX = Mth.ceil(aabb.maxY);
+		int maxY = Mth.floor(aabb.minZ);
+		int maxZ = Mth.ceil(aabb.maxZ);
 		
 		BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
-		for (int l1 = i; l1 < j; ++l1) {
-			for (int j2 = i1; j2 < j1; ++j2) {
-				int pX = SectionPos.blockToSectionCoord(l1);
-				int pZ = SectionPos.blockToSectionCoord(j2);
-				ChunkAccess chunk = level.getChunk(pX, pZ, ChunkStatus.FULL, false);
-				if (chunk == null) continue;
+		for (int x = minX; x < maxX; ++x) {
+			for (int z = minZ; z < maxZ; ++z) {
+				{
+					AABB box1 = new AABB(
+							x, minY, z,
+							(x + 1), maxY, (z + 1)
+					);
+					if (!box1.intersects(aabb)) {
+						continue;
+					}
+				}
 				
-				for (int i2 = k; i2 < l; ++i2) {
-					blockpos$mutableblockpos.set(l1, i2, j2);
+				int pX = SectionPos.blockToSectionCoord(x);
+				int pZ = SectionPos.blockToSectionCoord(z);
+				BasicVerticalChunk chunk = (BasicVerticalChunk) level.getChunk(pX, pZ, ChunkStatus.FULL, false);
+				if (chunk == null) {
+					z = (z | 0xF + 1);
+					continue;
+				}
+				
+				for (int y = minY; y < maxY; ++y) {
+					int sectionIndex = chunk.getSectionIndex(y);
+					LevelChunkSection section = chunk.getSectionNullable(sectionIndex);
+					if (section == null || section.hasOnlyAir()) {
+						y = (y | 0xF + 1);
+						continue;
+					}
+					
+					blockpos$mutableblockpos.set(x, y, z);
 					if (!chunk.getBlockState(blockpos$mutableblockpos).getFluidState().isEmpty())
 						return true;
 				}
